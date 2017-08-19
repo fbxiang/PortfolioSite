d0561ca938cfe569ec9fed0bc1198135f3c57ed3
fbxiang <fxiang2@illinois.edu>
2017-08-19 22:55:13 +0800

    start refactor

--- backend/index.ts
+++ backend/index.ts
@@ -1,33 +1,19 @@
 import * as express from 'express';
 import * as bodyparser from 'body-parser';
-
 import * as http from 'http';
-
 import * as fs from 'fs';
-import * as async from 'async';
 
 const app = express();
 
 import { portfolioPageRouter, portfolioRouter } from './routers/portfolio';
 
-import { blogPageRouter } from './routers/blog';
-import { userRouter } from './routers/user';
-
-// user body parser
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: true}));
-app.use('/api', express.static(__dirname + '/static'))
+app.use(bodyparser.urlencoded({ extended: true }));
+// app.use('/api', express.static(__dirname + '/static'))
 
 app.use(portfolioPageRouter);
-app.use(portfolioRouter);
-app.use('/api', blogPageRouter);
-app.use('/api/user', userRouter)
+app.use('/api', portfolioRouter);
 
-// app.get('/*', (req, res, next) => {
-//   next();
-// }, express.static(__dirname + '/static'));
-
-// create server
 const port = process.env.PORT ? process.env.PORT : 3000;
 const server = http.createServer(app);
 server.listen(port);
--- backend/routers/portfolio.ts
+++ backend/routers/portfolio.ts
@@ -1,120 +1,43 @@
 import * as express from 'express';
+import * as fs from 'fs';
+import { exists, readdir, readFile } from 'mz/fs';
 
 export const portfolioRouter = express.Router();
 export const portfolioPageRouter = express.Router();
+import { PortfolioData, getPortfolioSummaries } from '../models';
 
-import * as fs from 'fs';
-
-export interface PortfolioData {
-  id: string,
-  name: string,
-  filenames: string[]
-}
-
-function readdir(path:string) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(path, (err, files) => {
-      if (err)
-        reject(err);
-      else
-        resolve(files);
+portfolioRouter.get('/portfolio/summary', async (req, res, next) => {
+  getPortfolioSummaries()
+    .then(summaries => res.send(summaries))
+    .catch(err => {
+      res.status(500).send({ message: 'Portfolio list not available on the server' });
     });
-  })
-}
-
-function readFile(path:string) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, file) => {
-      if (err)
-        reject(err);
-      else
-        resolve(file);
-    })
-  })
-}
-
-function exists(path: string) {
-  return new Promise((resolve, reject) => {
-    fs.exists(path, exists => {
-      resolve(exists);
-    })
-  })
-}
-
-portfolioRouter.get('/portfolio', async (req, res, next) => {
-  const portfolioDir = `${__dirname}/../static/${req.query['name']}/portfolio/`;
-  let jsonFiles:string[] = await readdir(portfolioDir) as string[];
-  jsonFiles = jsonFiles.filter(f => f.includes('.json'));
-  let jsonData = await Promise.all(jsonFiles.map(async file => {
-    let data:string = await readFile(`${portfolioDir}${file}`) as string;
-    let {id, name} = JSON.parse(data) as PortfolioData;
-    return {id, name}
-  }));
-  res.send(jsonData);
 })
 
-portfolioPageRouter.get('/portfolio-page', async (req, res, next) => {
-  try {
-    const portfolioDir = `${__dirname}/../static/${req.query['name']}/portfolio/`
-    const jsonFile = `${portfolioDir}${req.query['id']}.json`;
-    let data = await readFile(jsonFile) as string;
-    let portfolioData = JSON.parse(data) as PortfolioData;
-    let allFiles = await Promise.all(portfolioData.filenames.map(async f => {
-      return await readFile(`${portfolioDir}${f}`);
-    }));
-    res.send(allFiles);
-  }
-  catch(err) {
-    res.status(404).send('Page does not exist yet.');
-  }
-})
+// portfolioRouter.get('/portfolio', async (req, res, next) => {
 
-portfolioPageRouter.post('/add-portfolio-page', async (req, res, next) => {
-  let {name, id, pageName} = req.body as {name:string, id: string, pageName: string};
-  name = name.trim();
-  id = id.trim();
-  pageName = pageName.trim();
+//   let jsonFiles: string[] = await readdir(PortfolioDir) as string[];
+//   jsonFiles = jsonFiles.filter(f => f.includes('.json'));
+//   let jsonData = await Promise.all(jsonFiles.map(async file => {
+//     let data = await readFile(`${portfolioDir}${file}`);
+//     let { id, name } = JSON.parse(data.toString()) as PortfolioData;
+//     return { id, name }
+//   }));
+//   res.send(jsonData);
+// })
 
-  const portfolioDir = `${__dirname}/../static/${name}/portfolio/`;
-  const jsonFileName = `${portfolioDir}${id}.json`;
-  if (await exists(jsonFileName)) {
-    return res.status(400).send('The id is duplicated. Please change the id.');
-  }
-  fs.writeFile(jsonFileName, JSON.stringify({
-    name: pageName,
-    id: id,
-    filenames: []
-  }), 'utf8', err => {
-    if (err)
-      res.send(400).send("Upload Failed");
-    else
-      res.send("success");
-  });
-})
-
-portfolioPageRouter.post('/change-portfolio-page', async (req, res, next) => {
-  let {name, id, index, content} = req.body as {name: string, id: string, index: number, content: string};
-
-  const portfolioDir = `${__dirname}/../static/${name}/portfolio/`;
-  const jsonFileName = `${portfolioDir}${id}.json`;
-
-  try {
-    let filename = (JSON.parse(await readFile(jsonFileName) as string) as PortfolioData).filenames[index];
-    let filepath = `${portfolioDir}${filename}`
-    if (await exists(filepath)) {
-      fs.writeFile(filepath, content, 'utf8', err => {
-        if (err)
-          throw err;
-        else
-          res.send('success');
-      })
-    }
-    else {
-      res.status(400).send('Invalid Index');
-    }
-  }
-  catch (err) {
-    console.log(err);
-    res.status(400).send('Invalid File');
-  }
-})
+// portfolioPageRouter.get('/portfolio-page', async (req, res, next) => {
+//   try {
+//     const portfolioDir = `${__dirname}/../static/${req.query['name']}/portfolio/`
+//     const jsonFile = `${portfolioDir}${req.query['id']}.json`;
+//     let data = await readFile(jsonFile);
+//     let portfolioData = JSON.parse(data.toString()) as PortfolioData;
+//     let allFiles = await Promise.all(portfolioData.filenames.map(async f => {
+//       return await readFile(`${portfolioDir}${f}`);
+//     }));
+//     res.send(allFiles);
+//   }
+//   catch (err) {
+//     res.status(404).send({ message: 'Page does not exist yet.' });
+//   }
+// })
